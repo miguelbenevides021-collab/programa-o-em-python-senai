259652d0e8cf76cbcbb933b6af6cb6913705cc85
miguelbenevides021-collab <miguelbenevides021@gmail.com>
2026-07-15 15:43:05 -0300

    Delete atividade6.py

--- atividade6.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('Digite seu numero'))
-if n1 > 0 and n1 < 10:
-    print('numero positivo')
-elif n1 >=10:
-    print('Numero superior a 10')
-else:
-    print('Numero nao encontrado!')
